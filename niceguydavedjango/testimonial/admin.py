4974d388fe4f2c946927b33e129ddaaee5885fc9
david.talbot <david.talbot@e9dac81e-0f1e-11df-b430-b3a03772fb55>
2010-02-01 19:23:43 +0000

    Add Person as app. Basic fields. Add to admin

--- niceguydavedjango/testimonial/admin.py
+++ niceguydavedjango/testimonial/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from niceguydavedjango.testimonials.models import Testimonial
+from niceguydavedjango.testimonial.models import Testimonial
 
 # =============================================================================
 
