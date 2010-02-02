0046a727b528ad50ed5d2ee6eff0f90022ea3b39
david.talbot <david.talbot@e9dac81e-0f1e-11df-b430-b3a03772fb55>
2010-02-02 12:31:23 +0000

    Add version control to news and testimonials (still need to work out why imports are unresolved in Eclipse though...)

--- niceguydavedjango/testimonial/admin.py
+++ niceguydavedjango/testimonial/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from niceguydavedjango.testimonial.models import Testimonial
+from reversion.admin import VersionAdmin
 
 # =============================================================================
 
-class TestimonialAdmin(admin.ModelAdmin):
+class TestimonialAdmin(VersionAdmin):
     list_display = ('name','creation_date')
 admin.site.register(Testimonial, TestimonialAdmin)
 
