70451af70a843503c23a837875f70dcaf15bdf1e
david.talbot <david.talbot@e9dac81e-0f1e-11df-b430-b3a03772fb55>
2010-02-04 17:08:39 +0000

    Inherit from Person base class. Remove db from svn

--- trunk/niceguydavedjango/settings.py
+++ trunk/niceguydavedjango/settings.py
@@ -11,8 +11,8 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'ngd.db'       # Or path to database file if using sqlite3.
+DATABASE_ENGINE = 'sqlite3'    
+DATABASE_NAME = 'C:/Dev/Django/db/ngd.sqlite'
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
