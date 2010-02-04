56867af4d548f036fe1819cbc16acd0d375e8899
david.talbot <david.talbot@e9dac81e-0f1e-11df-b430-b3a03772fb55>
2010-02-04 10:49:57 +0000

    Add person_type to model

--- trunk/niceguydavedjango/person/models.py
+++ trunk/niceguydavedjango/person/models.py
@@ -3,10 +3,17 @@ from django.db import models
 
 # =============================================================================
 
+TYPE_CHOICES = (
+    ('Student', 'Student'),
+    ('Staff', 'Staff'),
+    ('Guest', 'Visiting guest'),
+)
+
 class Person(models.Model):
     first_name      = models.CharField(max_length=100)
     last_name       = models.CharField(max_length=100)
     email           = models.EmailField()
+    person_type     = models.CharField(max_length=10, choices=TYPE_CHOICES)
     creation_date   = models.DateTimeField(default=datetime.datetime.today())
     active          = models.BooleanField(default=True)
     
