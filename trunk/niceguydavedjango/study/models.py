5d69104b43fe027a892aeae7cfdbfa527a60c6b8
david.talbot <david.talbot@e9dac81e-0f1e-11df-b430-b3a03772fb55>
2010-02-04 10:50:15 +0000

    Update other models

--- trunk/niceguydavedjango/study/models.py
+++ trunk/niceguydavedjango/study/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # =============================================================================
 
-class Department(models.Model):
-    name = models.CharField(max_length=100)
-    
-
 class Specialism(models.Model):
     name = models.CharField(max_length=100)
-
+    
+    
+class Department(models.Model):
+    name        = models.CharField(max_length=100)
+    specialism  = models.ManyToManyField(Specialism, blank=True)
